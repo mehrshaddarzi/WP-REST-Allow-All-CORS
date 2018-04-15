7c12ecc609ee6f8a8360c922aae91d3bb54d0be5
Wp developer <mehrshad198@gmail.com>
2018-04-15 23:27:40 +0430

    Update plugin.php

--- plugin.php
+++ plugin.php
@@ -37,3 +37,18 @@ function wp_rest_allow_all_cors() {
 		return $value;
 	});
 } // End fucntion wp_rest_allow_all_cors().
+
+
+/*
+function my_customize_rest_cors() {
+	remove_filter( 'rest_pre_serve_request', 'rest_send_cors_headers' );
+	add_filter( 'rest_pre_serve_request', function( $value ) {
+		header( 'Access-Control-Allow-Origin: *' );
+		header( 'Access-Control-Allow-Methods: GET' );
+		header( 'Access-Control-Allow-Credentials: true' );
+		header( 'Access-Control-Expose-Headers: Link', false );
+		return $value;
+	} );
+}
+add_action( 'rest_api_init', 'my_customize_rest_cors', 15 );
+*/
